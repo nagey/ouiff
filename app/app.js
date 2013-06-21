50a5bd11f3ed76a1473e8e46f4dfd77d4d2b108a
Stefan Nagey <stefan@nagey.com>
2013-06-21 09:58:56 -0400

    alter instagram callback URL

--- app/app.js
+++ app/app.js
@@ -85,7 +85,7 @@ server.listen(app.get('port'), function(){
 
 
 
-/*Instagram.set('client_id', '6527838fef5e4f0790a2aa7c9ddbc158');
+Instagram.set('client_id', '6527838fef5e4f0790a2aa7c9ddbc158');
 Instagram.set('client_secret', '80d7a8a4247e498ab6df545f9c5806d2');
 
 Instagram.tags.info({
@@ -98,5 +98,5 @@ Instagram.tags.info({
 var tagName = '15sfest';
 Instagram.tags.subscribe({ 
   object_id: tagName,
-  callback_url: "http://15sfest.com/instagram/callback?tag=" + tagName 
-});*/
+  callback_url: "http://15sfest.com/instagram/subscription/callback?tag=" + tagName 
+});
