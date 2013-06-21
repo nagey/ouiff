a79c046082b8a773c7d42115f3c60ce361e1635a
Stefan Nagey <stefan@nagey.com>
2013-06-21 10:14:42 -0400

    added post route

--- app/routes/instagram.js
+++ app/routes/instagram.js
@@ -8,4 +8,12 @@ module.exports = function (app) {
     console.log("Parameters", req.params);
     res.send(req.query["hub.challenge"]);
   });
+  
+  app.post('/instagram/subscription/:tagname/callback', function(req,res) {
+    console.log("Query", req.query);
+    console.log("Parameters", req.params);
+    res.send(req.query["hub.challenge"]);
+  });
+  
+
 }
