c0eb21a7129b7ee30b3c1de556ae341b7142bb13
Stefan Nagey <stefan@nagey.com>
2013-06-24 17:52:53 -0400

    chanegs in media service

--- html/app/scripts/app.js
+++ html/app/scripts/app.js
@@ -8,23 +8,23 @@ define([
   ], function ($, bootstrap, angular_blank, VideoListCtrl) {
     'use strict';
 
-    var app = angular.module("fest", []).config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
+    var app = angular.module("fest", ["ngResource"]).config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
       $locationProvider.html5Mode(true);
     }]);
 
-    app.controller("VideoListCtrl", ["$location", "$scope", VideoListCtrl]);
-    
+    app.controller("VideoListCtrl", ["$location", "$scope", "media", VideoListCtrl]);
     
     app.service("media", ["$resource", function ($resource) {
       var User = $resource('http://15sfest.com/media/:id', {id:'@id'});
+      console.log("media service");
       
       this.index = function () {
         var user = User.get({}, function() {
           console.log(user);
         });
       }
+      this.index();
     }]);
-    
     
     
     $(document).ready(function () {
