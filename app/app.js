cc0441e7356db4e3e0d87b09df4b40b5a551393b
Stefan Nagey <stefan@nagey.com>
2013-06-25 10:37:10 -0400

    intercept OPTIONS request

--- app/app.js
+++ app/app.js
@@ -48,11 +48,16 @@ app.extras.mongo = mongo;
 
 // CORS Config
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
     next();
+  }
 }
 app.use(allowCrossDomain);
 // END CORS Config
