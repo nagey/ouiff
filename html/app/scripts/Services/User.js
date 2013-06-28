a7d246754d53ff78a1f6a5e373e356621e4b2702
Stefan Nagey <stefan@nagey.com>
2013-06-28 11:49:17 -0400

    added callback to user status method

--- html/app/scripts/Services/User.js
+++ html/app/scripts/Services/User.js
@@ -25,17 +25,39 @@ define(['angular'], function (angular ) {
         }
       }
       
-      this.status = function () {
+      this.status = function (cb) {
         if (hasStatus) {
-          if (!loggedIn) return {"loggedIn": loggedIn};
-          else return {"loggedIn": loggedIn, "profile": userObj};
+          if (!loggedIn) {
+            if (typeof cb == 'function') {
+              cb({"loggedIn": loggedIn});
+            }
+            return {"loggedIn": loggedIn};
+          }
+          else {
+            if (typeof cb == 'function') {
+              cb({"loggedIn": loggedIn, "profile": userObj});
+            }
+            return {"loggedIn": loggedIn, "profile": userObj};
+          }
         }
         else {
           $http({method: 'GET', url: '/auth/status'}).
             success(function(data, status, headers, config) {
               if (data.status) {
                 hasStatus = true;
+                if (typeof cb == 'function') {
+                  cb({"loggedIn": loggedIn, "profile": userObj});
+                }
                 loginUser(data.user);
+              }
+              else {
+                if (typeof cb == 'function') {
+                  cb({"loggedIn": loggedIn});
+                }
+              }
+            }).error(function () {
+              if (typeof cb == 'function') {
+                cb({"loggedIn": loggedIn });
               }
             });
         }
