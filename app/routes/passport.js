650d3a079d5509ebc4c9c8817a39a37ad480660e
Stefan Nagey <stefan@nagey.com>
2013-07-01 15:00:09 -0400

    make sure session is defined when logging in users

--- app/routes/passport.js
+++ app/routes/passport.js
@@ -124,10 +124,12 @@ module.exports = function (app) {
       passReqToCallback: true
     },
     function (req, accessToken, refeshToken, profile, done) {
-      req.session.instagram = req.session.instagram || {};
-      req.session.instagram.accessToken = accessToken;
-      req.session.instagram.refeshToken = refeshToken;
-      fetchOrCreateUser(req, profile, done);
+      if (req.session) {
+        req.session.instagram = req.session.instagram || {};
+        req.session.instagram.accessToken = accessToken;
+        req.session.instagram.refeshToken = refeshToken;
+        fetchOrCreateUser(req, profile, done);
+      }
     }
   ));
   
@@ -140,10 +142,12 @@ module.exports = function (app) {
       passReqToCallback: true
     },
     function (req, accessToken, refreshToken, profile, done) {
-      req.session.facebook = req.session.facebook || {};
-      req.session.facebook.accessToken = accessToken;
-      req.session.facebook.refreshToken = refreshToken;
-      fetchOrCreateUser(req, profile, done);
+      if (req.session) {
+        req.session.facebook = req.session.facebook || {};
+        req.session.facebook.accessToken = accessToken;
+        req.session.facebook.refreshToken = refreshToken;
+        fetchOrCreateUser(req, profile, done);
+      }
     }
   ));
   
@@ -156,10 +160,12 @@ module.exports = function (app) {
       passReqToCallback: true
     },
     function (req, token, tokenSecret, profile, done) {
-      req.session.twitter = req.session.twitter || {};
-      req.session.twitter.tokenSecret = tokenSecret;
-      req.session.twitter.token = token;
-      fetchOrCreateUser(req, profile, done);
+      if (req.session) {
+        req.session.twitter = req.session.twitter || {};
+        req.session.twitter.tokenSecret = tokenSecret;
+        req.session.twitter.token = token;
+        fetchOrCreateUser(req, profile, done);
+      }
     }
   ));
   
