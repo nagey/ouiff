bfff720f3151a80b000dcf577428aef4947fa153
Stefan Nagey <stefan@nagey.com>
2013-07-01 16:03:25 -0400

    remove extraneous debug info

--- app/routes/passport.js
+++ app/routes/passport.js
@@ -78,11 +78,6 @@ module.exports = function (app) {
       else {
         console.log("User Object unchanged");
         if ((!req.user.tokens) || (!req.user.tokens[profile.provider]) || (req.user.tokens[profile.provider][0] != token1) || (req.user.tokens[profile.provider][1] != token2)) {
-          console.log((!req.user.tokens));
-          console.log((!req.user.tokens[profile.provider]));
-          console.log((req.user.tokens[profile.provider][0] != token1));
-          console.log((req.user.tokens[profile.provider][1] != token2));
-          console.log("setting tokens for "+profile.provider, token1, token2);
           req.user.tokens = req.user.tokens || {};
           req.user.tokens[profile.provider] = [token1, token2];
           var providerTokens = "tokens."+profile.provider;
