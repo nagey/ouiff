1ed5b6475314ecddc01dbe9e7c514a1704e68473
Stefan Nagey <stefan@nagey.com>
2013-07-09 17:34:01 -0400

    refactor "return result" to take cb, count and offset in and return a function def

--- html/app/scripts/Services/Media.js
+++ html/app/scripts/Services/Media.js
@@ -1,5 +1,5 @@
 /*global define */
-define(['angular'], function (angular ) {
+define(['angular'], function () {
     'use strict';
 
     return function Media($resource, $rootScope) {
@@ -8,9 +8,11 @@ define(['angular'], function (angular ) {
       var BestMedia = $resource('http://15sfest.com/media/top/:id', {id:'@id'});
       var userMedia = false; 
 
-      var returnResults = function(result) {
-        if (!count) count = result.length;
-        if (typeof cb === "function") cb(result.splice(offset,count));
+      var returnResults = function (cb, count, offset) {
+        return function(result) {
+          if (!count) count = result.length;
+          if (typeof cb === "function") cb(result.splice(offset,count));
+        };
       };
 
       $rootScope.$on("userLogin", function (user) {
@@ -19,21 +21,21 @@ define(['angular'], function (angular ) {
       
       this.index = function (cb, count, offset) {
         if (!offset) offset = 0;
-        MediaRsc.query(returnResults);
+        MediaRsc.query(returnResults(cb, count, offset));
       };
       
       this.bestOf = function (cb, count, offset) {
         if (!offset) offset = 0;
-        BestMedia.query(returnResults);
+        BestMedia.query(returnResults(cb, count, offset));
       };
 
       this.featured = function (cb, count, offset) {
         if (!offset) offset = 0;
-        FeaturedMedia.query(returnResults);
+        FeaturedMedia.query(returnResults(cb, count, offset));
       };
 
       this.mediaById = function (id, cb){
-        var clip = MediaRsc.get({id: id}, function(result) {
+        var clip = MediaRsc.get({id: id}, function() {
           if (typeof cb === "function") cb(clip);
           else return clip;
         });
@@ -41,28 +43,28 @@ define(['angular'], function (angular ) {
       
       this.mediaCurrentUser = function (cb, count, offset) {
         if (!offset) offset = 0;
-        userMedia.query(returnResults);
+        userMedia.query(returnResults(cb, count, offset));
       };
       
       this.mediaByUser = function (username, cb, count, offset) {
         if (!offset) offset = 0;
         var mediaResource = $resource("http://15sfest.com/media/user/"+username, {id: "@id"});
-        mediaResource.query(returnResults);
+        mediaResource.query(returnResults(cb, count, offset));
       };
       
       this.mediaByTag = function (tag, cb, count, offset) {
         var tagMedia = $resource("http://15sfest.com/media/tag/"+tag, {id: "@id"});
-        tagMedia.query(returnResults);
+        tagMedia.query(returnResults(cb, count, offset));
       };
       
       this.topMediaByTag = function (tag, cb, count, offset) {
         var tagMedia = $resource("http://15sfest.com/media/tag/"+tag+"/top", {id: "@id"});
-        tagMedia.query(returnResults);
+        tagMedia.query(returnResults(cb, count, offset));
       };
       
       this.tags = function (cb, count, offset) {
         var tagService = $resource("http://15sfest.com/media/tag", {id: "@id"});
-        tagService.query(returnResults);
+        tagService.query(returnResults(cb, count, offset));
       };
 
     };
