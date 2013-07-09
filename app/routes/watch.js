0a83f9b6992a780363cabe863283db0c4d171157
Stefan Nagey <stefan@nagey.com>
2013-07-09 10:16:11 -0400

    basic scaffold for social watch url

---
+++ app/routes/watch.js
@@ -0,0 +1,15 @@
+module.exports = function (app) {
+  app.get("/social/watch/:media", function (req, res) {
+    app.extras.stathat.track("social watch", 1);  
+    app.extras.mongo.media.find({id: req.params.media}, function (err, docs) {
+      if (err) {
+        app.extras.stathat.track("db error", 1);
+        res.redirect("http://15sfest.com");
+      }
+      else {
+        res.render("watch", { media: docs[0] });      
+      }
+    });
+  });
+  
+}
