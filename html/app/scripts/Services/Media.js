ea11486709727e92140ddf0ef0a70d211011485c
Stefan Nagey <stefan@nagey.com>
2013-07-09 11:38:11 -0400

    de-lint

--- html/app/scripts/Services/Media.js
+++ html/app/scripts/Services/Media.js
@@ -3,7 +3,7 @@ define(['angular'], function (angular ) {
     'use strict';
 
     return function Media($resource, $rootScope) {
-      var Media = $resource('http://15sfest.com/media/:id', {id:'@id'});
+      var MediaRsc = $resource('http://15sfest.com/media/:id', {id:'@id'});
       var FeaturedMedia = $resource('http://15sfest.com/media/featured/:id', {id:'@id'});
       var BestMedia = $resource('http://15sfest.com/media/top/:id', {id:'@id'});
       var userMedia = false; 
@@ -13,30 +13,29 @@ define(['angular'], function (angular ) {
       });
       
       this.index = function (cb, count) {
-        Media.query(function(result) {
+        MediaRsc.query(function(result) {
           if (typeof cb === "function") cb(result.splice(0,count));
         });
-      }
+      };
       
       this.bestOf = function (cb, count) {
         BestMedia.query(function (result) {
           if (typeof cb === "function") cb(result.splice(0,count));
         });
-      }
+      };
 
       this.featured = function (cb, count) {
         FeaturedMedia.query(function (result) {
           if (typeof cb === "function") cb(result.splice(0,count));
         });
-      }
+      };
 
       this.mediaById = function (id, cb){
-        var clip = Media.get({id: id}, function(result) {
+        var clip = MediaRsc.get({id: id}, function(result) {
           if (typeof cb === "function") cb(clip);
           else return clip;
         });
-      }
+      };
 
-      //this.index();
-    }
+    };
 });
