8f3efeac619219d25fa57d395d334d5fb72c57e7
Stefan Nagey <stefan@nagey.com>
2013-07-10 13:47:53 -0400

    remove unused controllers

--- html/app/scripts/Controllers/UserCtrl.js
+++
@@ -1,8 +0,0 @@
-/*global define */
-define(['angular', 'jquery'], function () {
-    'use strict';
-
-    return function UserCtrl($scope, $routeParams) {
-        console.log('UserCtrl',$routeParams);
-      };
-  });
