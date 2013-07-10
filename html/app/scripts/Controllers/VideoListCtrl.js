b0087f48746a268064112bb2d9c32d7dd33aebb8
Stefan Nagey <stefan@nagey.com>
2013-07-10 17:32:12 -0400

    Refactor watch.html, switch flags to sprite to work with imgmin

--- html/app/scripts/Controllers/VideoListCtrl.js
+++ html/app/scripts/Controllers/VideoListCtrl.js
@@ -7,15 +7,33 @@ define(['angular', 'jquery', 'angular-modal'], function () {
       media.index();
 
       $scope.modal = {};
+
+      $scope.videos = [];
+
       //console.log("$location.parse(url):",$location.$$parse("http://15sfest.com/watch/489918443045812158_582588") );
       media.bestOf(function (result) {
-        $scope.bestOfList = result;
+        $scope.videos[0] = {
+            title: "Highest Rated",
+            css: "best-of",
+            span: 6,
+            videos: result
+          };
       },4);
       media.featured(function (result) {
-        $scope.featuredList = result;
+        $scope.videos[1] = {
+          title: "Featured", 
+          css: "featured",
+          span: 3,
+          videos: result
+        };
       },2);
       media.index(function (result) {
-        $scope.latestList = result;
+        $scope.videos[2] = {
+          title: "Latest",
+          css: "latest",
+          span: 3,
+          videos: result
+        };
       },2);
 
       $scope.imgClick = function (item){
