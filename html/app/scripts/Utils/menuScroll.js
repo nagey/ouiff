bdcf4c504151be3b82321214d2ed832ebef5305c
Stefan Nagey <stefan@nagey.com>
2013-07-11 15:48:09 -0400

    put into rjs format

--- html/app/scripts/Utils/menuScroll.js
+++ html/app/scripts/Utils/menuScroll.js
@@ -1,13 +1,18 @@
-$(document).ready(function(){
+/*global define */
+define(["jquery"], function ($) {
   "use strict";
-  var links = $('#main-nav li a.scroll-btn');
-  links.click(function (e) {
-    e.preventDefault();
-    goToByScroll($(this).attr('data'));
+  $(document).ready(function(){
+    var links = $('#main-nav li a.scroll-btn');
+
+    var goToByScroll = function (dataslide) {
+      $('html,body').animate({
+        scrollTop: $("#"+dataslide).offset().top
+      }, 500/*, 'easeInOutQuint'*/);
+    };
+
+    links.click(function (e) {
+      e.preventDefault();
+      goToByScroll($(this).attr('data'));
+    });
   });
-  function goToByScroll(dataslide) {
-    $('html,body').animate({
-      scrollTop: $("#"+dataslide).offset().top
-    }, 500/*, 'easeInOutQuint'*/);
-  }
 });
