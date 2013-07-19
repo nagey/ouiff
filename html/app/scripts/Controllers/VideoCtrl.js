a11e59b0ac6f4f2038236c4d72fe922f443544b0
Stefan Nagey <stefan@nagey.com>
2013-07-19 17:06:42 -0400

    made VideoCtrl + template more "angular"

--- html/app/scripts/Controllers/VideoCtrl.js
+++ html/app/scripts/Controllers/VideoCtrl.js
@@ -2,35 +2,37 @@
 define(['jquery', 'angular'], function ($) {
     'use strict';
 
-    var VideoCtrl = function ($scope) {
-        $scope.bindListener = function(){
+    var VideoCtrl = function ($scope, user, media) {
 
-          $('#play-btn, #videoPlayer').bind('click',function(){
-            var video = document.getElementById("videoPlayer");
-            if($('#control-window').hasClass('playing')) {
-              video.pause();
-            }
-            else {
-              video.play();
-            }
-
-            $('#control-window').toggleClass('playing');
-          });
-
-          $('#mute-btn').bind('click',function(){
-            if($('#control-window').hasClass('muted')) {
-              $('#videoPlayer').prop('muted', false);
-            }
-            else {
-              $('#videoPlayer').prop('muted', true);
-            }
-
-            $('#control-window').toggleClass('muted');
-          });
-        };
       };
 
     VideoCtrl.$inject = ["$scope"];
+      $scope.playVideo = function() {
+        var video = document.getElementById("videoPlayer");
+        if($('#control-window').hasClass('playing')) {
+          video.pause();
+        }
+        else {
+          video.play();
+        }
+
+        $('#control-window').toggleClass('playing');
+      };
+
+      $scope.muteVideo = function(){
+        if($('#control-window').hasClass('muted')) {
+          $('#videoPlayer').prop('muted', false);
+        }
+        else {
+          $('#videoPlayer').prop('muted', true);
+        }
+
+        $('#control-window').toggleClass('muted');
+      };
+
+
+    };
+
 
     return VideoCtrl;
   });
